3695f494144445ba2896e6320f09ce4d73293a29
flovnes <whyflovnes@gmail.com>
2024-05-04 12:05:53 +0300

    h

--- Main.cs
+++ Main.cs
@@ -8,7 +8,7 @@ namespace Lab5
     static void Main()
     {
       Console.OutputEncoding = UTF8Encoding.UTF8;    
-      bool get_out_out = false;
+      bool get_out_out = false, get_out = false;
       while (!get_out_out) {
         Console.Write("\nChoose struct { Order, Train }: ");
         Console.WriteLine("[0] Exit");
@@ -18,7 +18,7 @@ namespace Lab5
         
         switch (objectType) {
           case 1:
-            bool get_out = false;
+            get_out = false;
             while (!get_out)
             {
               Console.WriteLine("[0] Exit");
@@ -65,45 +65,49 @@ namespace Lab5
             }
           break;
           case 2:
-            Console.WriteLine("[0] Exit");
-            Console.WriteLine("[1] Write data to TXT file");
-            Console.WriteLine("[2] Serialize data to JSON file");
-            Console.WriteLine("[3] Serialize data to XML file");
-            if (File.Exists("Train.txt"))
-              Console.WriteLine("[4] Read data data TXT file");
-            if (File.Exists("Train.json"))
-              Console.WriteLine("[5] Deserialize data from JSON file");
-            if (File.Exists("Train.xml"))
-              Console.WriteLine("[6] Deserialize data from XML file");
-              
-            Console.Write("\nChoose an option: ");
-            choice = int.Parse(Console.ReadLine());
-            switch (choice)
+            get_out = false;
+            while (!get_out)
             {
-              case 1:
-                WriteIntoTxt(EnterObjects<Train>(), "Train.txt");
-                break;
-              case 2:
-                SerializeIntoJson(EnterObjects<Train>(), "Train.json");
-                break;
-              case 3:
-                SerializeIntoXml(EnterObjects<Train>(), "Train.xml");
-                break;
-              case 4:
-                ReadFromTxt<Train>("Train.txt");
-                break;
-              case 5:
-                DeserializeFromJson<Train>("Train.json");
-                break;
-              case 6:
-                DeserializeFromXml<Train>("Train.xml");
-                break;
-              case 0:
-                get_out = true;
-                break;
-              default:
-                Console.WriteLine("?");
-                break;
+              Console.WriteLine("[0] Exit");
+              Console.WriteLine("[1] Write data to TXT file");
+              Console.WriteLine("[2] Serialize data to JSON file");
+              Console.WriteLine("[3] Serialize data to XML file");
+              if (File.Exists("Train.txt"))
+                Console.WriteLine("[4] Read data data TXT file");
+              if (File.Exists("Train.json"))
+                Console.WriteLine("[5] Deserialize data from JSON file");
+              if (File.Exists("Train.xml"))
+                Console.WriteLine("[6] Deserialize data from XML file");
+                
+              Console.Write("\nChoose an option: ");
+              choice = int.Parse(Console.ReadLine());
+              switch (choice)
+              {
+                case 1:
+                  WriteIntoTxt(EnterObjects<Train>(), "Train.txt");
+                  break;
+                case 2:
+                  SerializeIntoJson(EnterObjects<Train>(), "Train.json");
+                  break;
+                case 3:
+                  SerializeIntoXml(EnterObjects<Train>(), "Train.xml");
+                  break;
+                case 4:
+                  ReadFromTxt<Train>("Train.txt");
+                  break;
+                case 5:
+                  DeserializeFromJson<Train>("Train.json");
+                  break;
+                case 6:
+                  DeserializeFromXml<Train>("Train.xml");
+                  break;
+                case 0:
+                  get_out = true;
+                  break;
+                default:
+                  Console.WriteLine("?");
+                  break;
+              }
             }
           break;
           case 0:
